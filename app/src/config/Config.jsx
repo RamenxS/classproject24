b2240d328bc4268606f628a08d8faadcb6987c1f
RamenxS <ramenslewo1@gmail.com>
2024-09-23 10:55:26 +0000

    adding sign up and firebase app

---
+++ app/src/config/Config.jsx
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAWdLODpq2PtZdhi9RUHCAsLi-nJyMKHtk",
+    authDomain: "classproject24-389ce.firebaseapp.com",
+    projectId: "classproject24-389ce",
+    storageBucket: "classproject24-389ce.appspot.com",
+    messagingSenderId: "713602291981",
+    appId: "1:713602291981:web:aa8a57cfc15d579833b844"
+};
